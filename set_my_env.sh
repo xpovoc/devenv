4051c4e42d54c89685970c83a4d4d2444e1bf31a
rgounelle <rg0687@gmail.com>
2015-07-08 10:09:34 +0200

    fix .vim issue

--- set_my_env.sh
+++ set_my_env.sh
@@ -1,5 +1,6 @@
 echo "Setting env ..."
 sudo ln -sf ~/devenv/.bashrc ~/.bashrc 
 sudo ln -sf ~/devenv/.vimrc ~/.vimrc 
-sudo ln -sf ~/devenv/.vim ~/.vim 
+#sudo ln -sf ~/devenv/.vim ~/.vim 
+sudo cp -R ~/.vim ~/devenv/.vim 
 echo "... DONE !!!"
